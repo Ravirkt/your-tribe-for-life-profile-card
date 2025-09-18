08fc66d35b03ab5266d315c77907db430aa566ec
Ravirkt <r_tjikhoeri@live.com>
2025-09-18 21:28:48 +0200

    Feature: added home route and component for practice

---
+++ visitekaart/src/lib/components/CardGrid.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+
+
+<section class="main-container-cards">
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+    <div class="grid-card"></div>
+</section>
+
+
+
+<style>
+
+    .main-container-cards {
+        columns: 10rem;
+    }
+
+
+    .grid-card {
+        height: 10rem;
+        width: 5rem;
+        background-color: yellow;
+    }
+
+
+
+</style>
---
+++ visitekaart/src/lib/components/Header.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/about">about</a>
+	<a href="/home">home</a>
+	<a href="/details">details</a>
+</nav>
+
+
